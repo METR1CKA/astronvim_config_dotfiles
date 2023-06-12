247b5d0ee516d373056f348409ba701257abe242
METR1CKA <ferchosalazar054@gmail.com>
2023-06-11 18:35:25 -0600

    [UPD] - Format

--- plugins/user.lua
+++ plugins/user.lua
@@ -134,16 +134,16 @@ return {
 
   -- Indent Blankline
   {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      vim.opt.list = true
-
-      require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-      }
-    end,
+    -- "lukas-reineke/indent-blankline.nvim",
+    -- config = function()
+    --   vim.opt.list = true
+    --
+    --   require("indent_blankline").setup {
+    --     space_char_blankline = " ",
+    --     show_current_context = true,
+    --     show_current_context_start = true,
+    --   }
+    -- end,
   },
 
   -- Syntax
