9f641c19c9d5741e241a68b93884e37b7fb662b9
METR1CKA <ferchosalazar054@gmail.com>
2023-06-30 15:37:33 -0600

    [UPD] - Delete bufferline plugin in my astro-nvim config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,9 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- bars-and-lines
-  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
-
   -- Indent
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.indent.indent-blankline-nvim" },
