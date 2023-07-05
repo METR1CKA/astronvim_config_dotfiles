094ed4439392646234db7e1abb9b3079ed1c7043
METR1CKA <ferchosalazar054@gmail.com>
2023-07-04 21:47:04 -0600

    [UPD] - Bufferline config removed

--- plugins/user.lua
+++ plugins/user.lua
@@ -96,40 +96,6 @@ return {
     end,
   },
 
-  -- Bars And Lines
-
-  -- BufferLine
-  {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      vim.opt.termguicolors = true
-      require("bufferline").setup {
-        options = {
-          mode = "buffers",
-          themable = true,
-          diagnostics = "nvim_lsp",
-          diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local icon = level:match "error" and " " or " "
-            return " " .. icon .. count
-          end,
-          buffer_close_icon = "󰅖",
-          modified_icon = "●",
-          close_icon = "",
-          left_trunc_marker = "",
-          right_trunc_marker = "",
-          separator_style = "padded_slant",
-          hover = {
-            enabled = true,
-            delay = 100,
-            reveal = { "close" },
-          },
-        },
-      }
-    end,
-  },
-
   -- Indent
 
   -- Indent Blankline
